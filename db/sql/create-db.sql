2ceed4f734f4aa6c543e25a5186cc681b16e2281
Kalyanbabu007 <kalyanbabu1710@gmail.com>
2025-04-08 10:33:36 +0530

    Update create-db.sql

--- db/sql/create-db.sql
+++ db/sql/create-db.sql
@@ -41,7 +41,7 @@ INSERT INTO product (product_name, description, price, product_group, image_url)
 	, ('Muffin', 'Banana bread, blueberry, cranberry or apple', 3.00, 1, 'images/Muffins.jpg')
 	, ('Strawberry Blueberry Tart', 'Bursting with the taste and aroma of fresh fruit', 3.50, 1, 'images/Strawberry-Blueberry-Tarts.jpg')
     , ('Strawberry Tart', 'Made with fresh ripe strawberries and a delicious whipped cream', 3.50, 1, 'images/Strawberry-Tarts.jpg')
-	, ('Coffee', 'Freshly-ground black or blended Columbian coffee', 3.00, 2, 'images/Coffee.jpg')
+	, ('Chocolate Coffee', 'Freshly-ground black or blended Columbian coffee', 3.00, 2, 'images/Coffee.jpg')
 	, ('Hot Chocolate', 'Rich and creamy, and made with real chocolate', 3.00, 2, 'images/Cup-of-Hot-Chocolate.jpg')
 	, ('Latte', 'Offered hot or cold and in various delicious flavors', 3.50, 2, 'images/Latte.jpg');
 
